a01d2415271668a71f36a21c0f37f5c3c70d43cf
Wilberth Gonzalez <willgxnzalez@icloud.com>
2024-01-27 17:28:45 -0800

    even-odd two-pointers

---
+++ two-pointers/even-odd.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int * evenOdd(int *arr, int len) {
+    int left = 0;
+    int right = len - 1;
+    while (left < right) {
+        while (left < right && arr[left] % 2 == 0) // move left ptr to first odd
+            ++left;
+        while (left < right && arr[right] % 2 == 1) // move right ptr to first even
+            --right;
+        if (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+        }
+    }
+    return arr;
+}
+
+int main(int argc, char *argv[]) {
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int len = 10;
+    int * result = evenOdd(arr, len);
+    for (int i = 0; i < len; ++i) {
+        std::cout << result[i] << "\n";
+    }
+}
